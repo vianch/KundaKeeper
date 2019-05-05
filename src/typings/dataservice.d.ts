754f18f790505f59906a86f297817df0dd679a39
Victor J. Salazar <vagol942@gmail.com>
2019-05-05 09:00:15 -0500

    Adds minka endpoint services

--- src/typings/dataservice.d.ts
+++ src/typings/dataservice.d.ts
@@ -6,3 +6,31 @@ interface BuyPostData {
   handle: string;
   amountString: string;
 }
+
+interface GetBalanceData {
+  handle: string;
+}
+
+interface PostPaymentData {
+  sourceHandle: string;
+  targetHandle: string;
+  amountString: string;
+}
+
+interface BuyResponse {
+  id: string;
+  labels: {
+    status: string;
+  };
+}
+
+interface BalanceResponse {
+  amount: string;
+}
+
+interface PaymentResponse {
+  id: string;
+  labels: {
+    status: string;
+  };
+}
